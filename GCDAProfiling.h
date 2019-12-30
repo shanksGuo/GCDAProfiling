0fdf049e4b66fa32d4c4791702b007425662556e
shanks <gyjjone@163.com>
2019-12-30 21:52:58 +0800

    feat: add GCDAProfiling

---
+++ GCDAProfiling.h
@@ -0,0 +1,15 @@
+//
+//  GCDAProfiling.h
+//  IBUWireless
+//
+//  Created by shanks on 2019/8/14.
+//  Copyright © 2019 Ctrip. All rights reserved.
+//
+
+#ifndef GCDAProfiling_h
+#define GCDAProfiling_h
+
+#include <stdio.h>
+void __gcov_flush();
+
+#endif /* GCDAProfiling_h */
